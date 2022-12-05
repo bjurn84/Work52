692ea26a20cbf1b8c9fa435ae192ab7f1eba888a
bjurn84 <115032837+bjurn84@users.noreply.github.com>
2022-12-05 07:34:17 +0400

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("Введите  m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите  n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+double average = 0;
+double [,] matrix = new double [m,n];
+for(int i = 0; i < m; i++)
+{
+    for(int j = 0; j < n; j++)
+    {
+        matrix[i,j] = new Random().Next(0,11);
+        Console.Write(matrix[i,j] + " ");
+    }
+    Console.WriteLine();
+}
+for(int j = 0; j < n; j++)
+{
+    for(int i = 0; i < m; i++)
+    {
+        average+=matrix[i,j];        
+    }
+    Console.Write(Math.Round(average/m, 1) + "; ");
+    average = 0;
+}
